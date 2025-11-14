9e03e69eb1d86cbd0f3c40e0532ec65cf5a4a234
collinvictor1818 <moraaf770@gmail.com>
2025-11-14 15:52:21 +0300

    Initial Commit

---
+++ Veriable AppTests/Veriable_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  Veriable_AppTests.swift
+//  Veriable AppTests
+//
+//  Created by Imani on 14/11/2025.
+//
+
+import Testing
+@testable import Veriable_App
+
+struct Veriable_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
